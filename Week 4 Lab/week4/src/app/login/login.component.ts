4a1bbba318c8066eeddfd2097eaf53f1b5e32fcd
Mitchell Gorham <mitchell.gorham@griffithuni.edu.au>
2019-08-02 12:00:35 +1000

    Finished Week 4 Lab

--- Week 4 Lab/week4/src/app/login/login.component.ts
+++ Week 4 Lab/week4/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
+  email=""
+  pass=""
+  valid= [
+    {username:"abc@com", pwd:"123"},
+    {username:"def@com", pwd:"456"},
+    {username:"ghi@com", pwd:"789"}
+  ]
+
+  loginClick() {
+    this.router.navigate(["/account"]);
+    for (var i = 0; i < this.valid.length; i++) {
+      if( this.email == this.valid[i].username && this.pass == this.valid[i].pwd ){
+
+      }
+
+    }
+  }
   ngOnInit() {
   }
-
 }
+
+
